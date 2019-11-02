e7663177806b39c252d9fd3af06727a4b9aa97e8
Rachel Graham <rachelgraham@Rachels-MacBook-Pro.local>
2019-11-02 10:00:36 -0500

    created game logic

--- memory game/js/main.js
+++ memory game/js/main.js
@@ -1,6 +1,18 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+// Store queen in cardsInPlay
+// Store king in cardsInPlay
+
+cardsInPlay.push('queen');
+cardsInPlay.push('king')
+console.log("User flipped queen");
+console.log("User flipped king");
+
+// If both cards stored into cardsInPlay; page will alert "You found a match!"
+
+if (cardsInPlay.length === 2){
+	alert("You found a match!")
+};

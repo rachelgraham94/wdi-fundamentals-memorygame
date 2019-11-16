a97c9ef912fd7668a8c55f6f7016f5ca3fa5d969
Rachel Graham <rachelgraham@Rachels-MacBook-Pro.local>
2019-11-16 13:04:54 -0600

    created game logic

--- memory game/js/main.js
+++ memory game/js/main.js
@@ -1,31 +1,46 @@
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+	rank: 'queen',
+	cardImage: 'images/queen-of-hearts.png',
+	suit: 'hearts'
+},
+{
+	rank: 'queen',
+	cardImage: 'images/queen-of-diamonds.png',
+	suit: 'diamonds'
+},
+{
+	rank: 'king',
+	cardImage: 	'images/king-of-hearts.png',
+	suit: 'hearts'
+},
+{
+	rank: 'king',
+	cardImage: 'images/king-of-diamonds.png',
+	suit: 'diamonds'
+}
+];
 const cardsInPlay = [];
-var cardId = cards[0, 1, 2, 3]
 ///add the card that the user flipped
-cardsInPlay.push(cards[cardId]);
-///create a function to store all steps that should happen when the user flips a card.
+cardsInPlay.push(cards['queen']);
+cardsInPlay.push(cards['king']);
+///check for a match
 function checkForMatch() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
+	if (cardsInPlay[0] === cardsInPlay[2]) {
+  alert("You found a match!");
 } else {
-  console.log("Sorry, try again.");
+  alert("Sorry, try again.");
 }
 }
-
+///create a function to store all steps that should happen when the user flips a card.
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId])
+	console.log("User flipped " + cards[cardId].rank)
 	
 };
 flipCard(0);
 flipCard(2);
 checkForMatch();
-//var cardOne = cards[0];
-//cardsInPlay.push('cardOne');
-//console.log("User flipped " + cardOne);
 
-//var cardTwo = cards[2];
-//cardsInPlay.push('cardTwo')
-//console.log("User flipped " + cardTwo);
 
 
 
